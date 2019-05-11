36da0e20e86949bbc10be80a1d3a6756729d7a65
Bilal <bilaltehseen@gmail.com>
2019-05-11 05:25:53 -0700

    Python Basic

---
+++ Python_Basic.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Print Functions
+
+# In[25]:
+
+
+print("Hello Wolrd") ##Single String Print Function
+
+
+# In[26]:
+
+
+print("Hello World","Your Name","How are You") ##Multiple String
+
+
+# In[27]:
+
+
+print("Hello Wolrd","How are you all","Welcome to Python",sep="\n") ##Print Fn with sepration between string 
+
+
+# In[29]:
+
+
+print("Hello Wolrd","How are you all","Welcome to Python",end="-->") ##Print Fn with ending at last line for string 
+
+
+# ## Variables
+
+# In[38]:
+
+
+a=10; ##Decleartion of Variables
+
+
+# In[39]:
+
+
+print(a) ##Printing a
+
+
+# In[40]:
+
+
+b=["Your Name","Your Age","Your Work"] ##List type Variable
+
+
+# In[45]:
+
+
+print(b[1]) ##Printing list with index 1
+
+
+# In[46]:
+
+
+c=("John",19,"Data Scientest") ##Tussle, Type of Variable to store multiple information but unchangable during runtime
+
+
+# In[47]:
+
+
+print(c[1]) ##Printing tussle with index 1
+
+
+# In[48]:
+
+
+d={"Name1":"John","Name2":"Becky","Name3":"Cliff"} ##Dictionary ,Type of Variable to store multiple info with seprate tags
+
+
+# In[59]:
+
+
+print(d["Name2"]) ##Printing Specific Name1 from dictionary
+
+
+# ## Control Flow
+if Your logic:
+    Statement
+else:
+    Statement:
+# In[66]:
+
+
+Number=10;
+
+
+# In[67]:
+
+
+if Number >10:
+    print("Number is greater than 10")
+else:
+    print("Number not greater than 10")
+
+
+# In[63]:
+
+
+Number=20; ## *Value Changed*
+
+
+# In[65]:
+
+
+if Number >10:
+    print("Number is greater than 10")
+else:
+    print("Number not greater than 10")
+
+
+# In[ ]:
+
+
+
+
